34788b13006d6b4780c38fc87d8964365b184ab9
Szymon Popiolek <2004szymonp@gmail.com>
2022-01-18 10:46:48 +0100

    Working sound

---
+++ index.js
@@ -0,0 +1,63 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+console.log(numberOfDrumButtons.length);
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+
+	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+		let buttonInnerHTML = this.innerHTML
+
+		makesound(buttonInnerHTML)
+
+	});
+}
+
+document.addEventListener("keypress", function(event) {
+
+	makesound(event.key)
+
+});
+
+function makesound(key) {
+
+	switch (key) {
+		case "w":
+			let tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+	
+		case "a":
+			let tom2 = new Audio("sounds/tom-2.mp3");
+			tom2.play();
+			break;
+
+		case "s":
+			let tom3 = new Audio("sounds/tom-3.mp3");
+			tom3.play();
+			break;
+		
+		case "d":
+			let tom4 = new Audio("sounds/tom-4.mp3");
+			tom4.play();
+			break;
+
+		case "j":
+			let crash = new Audio("sounds/crash.mp3");
+			crash.play();
+			break;
+
+		case "k":
+			let kick = new Audio("sounds/kick.mp3");
+			kick.play();
+			break;
+
+		case "l":
+			let snare = new Audio("sounds/snare.mp3");
+			snare.play();
+			break;
+
+		default:
+			console.log(buttonInnerHTML);
+			break;
+	}
+}
